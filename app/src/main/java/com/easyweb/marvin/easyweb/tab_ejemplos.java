92ea42c3af0b6c79b7e0d0e9280b92e20808be29
Marvin Eliosa <marvineliosa@hotmail.com>
2017-11-17 18:46:05 -0600

    Se ha cambiado el TabHost por Tabs con Tabbed, este usa fragments y mejora el rendimiento de la aplicacion aunque solo es el cascaron, falta agregarle los ListView

---
+++ app/src/main/java/com/easyweb/marvin/easyweb/tab_ejemplos.java
@@ -0,0 +1,23 @@
+package com.easyweb.marvin.easyweb;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by marvin on 17/11/2017.
+ */
+
+public class tab_ejemplos extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_ejemplos, container, false);
+
+        return rootView;
+    }
+
+}
